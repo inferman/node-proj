282856354de445f1cb497479787b7145dceadf04
inferman <infernal.manowar@gmail.com>
2020-06-05 17:41:50 +0300

    added patch and delete tour endpoints

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/api/v1/tours', (req, res) => {
 app.get('/api/v1/tours/:id', (req, res) => {
   const id = +req.params.id;
   const tour = tours.find(item => item.id === id);
-  if(!tour) {return res.status(400).json({status:'fail', message: 'Data wasn\'t found'})}
+  if(!tour) {return res.status(404).json({status:'fail', message: 'Data wasn\'t found'})}
   res
     .status(200)
     .json({
@@ -30,7 +30,37 @@ app.get('/api/v1/tours/:id', (req, res) => {
         tour
       }
     })
+})
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = +req.params.id;
+  const isIndex = !!(~tours.findIndex(item => item.id === id));
+  if(!isIndex) return res.status(404).json({status:'fail', message: 'wrong id'});
+
+  let upd;
+  const updatedTours = tours.map(tour => tour.id === id ? (upd = {...tour, ...req.body},upd) : tour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(updatedTours), (err) => {
+    if(err) throw Error('something went wrong')
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: upd
+      }
+    });
+  })
+})
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = +req.params.id;
+  const isIndex = !!(~tours.findIndex(item => item.id === id));
   
+  if(!isIndex) return res.status(404).json({status:'fail', message: 'wrong id'})
+  const updatedTours = tours.filter(tour => tour.id !== id);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(updatedTours), (err) => {
+    res.status(204).json({status: 'success', data: null});
+  });
+
+
 })
 
 app.post('/api/v1/tours', (req, res) => {
@@ -47,8 +77,8 @@ app.post('/api/v1/tours', (req, res) => {
       }
     });
   });
-  
 })
+
 
 const port = 3000;
 app.listen(port, () => {
