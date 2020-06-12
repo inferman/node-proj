58ecce7660aa553cb9fc1e53e8c78e20b9818d4c
inferman <infernal.manowar@gmail.com>
2020-06-12 16:16:07 +0300

    added mongoose to project

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-const env =  require('dotenv');
-env.config({path: './config.env'});
+const mongoose = require('mongoose');
+const dotenv =  require('dotenv');
+
+dotenv.config({path: './config.env'});
+
+mongoose.connect(process.env.DATABASE_LOCAL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(_ =>  console.log('DB connection successful!'))
 
 const app = require('./app');
 
